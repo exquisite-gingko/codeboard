b72e57eaa705809e833f834a7025e9c9868aa86b
Anna Rogers <2muddybarefeet@gmail.com>
2015-11-20 07:40:27 -0800

    Deployed to AWS take two

--- db/config.js
+++ db/config.js
@@ -3,11 +3,12 @@
 // ##### [Back to Table of Contents](./tableofcontents.html)
 var mongoose = require('mongoose');
 
-// Currently configured for deployment. Change to this for development:
+if (process.env.MONGOLAB_URI) {
+  mongoose.connect(process.env.MONGOLAB_URI);
+} else {
+  mongoose.connect('mongodb://127.0.0.1');
+}
 
-// ```mongoose.connect('mongodb://127.0.0.1');```
-// mongoose.connect(process.env.MONGOLAB_URI);
-mongoose.connect('mongodb://127.0.0.1');
 
 var db = mongoose.connection;
 
