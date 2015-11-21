c794be33f23516e9abc45ebbd26a4605d5c86a18
Aaron Ackerman <a.ackerman050@gmail.com>
2015-11-20 17:16:21 -0800

    Modified controllers and factory into IFFE and refactored for readability

--- public/js/whiteboard.js
+++ public/js/whiteboard.js
@@ -3,57 +3,60 @@
 // ##### [Back to Table of Contents](./tableofcontents.html)
 
 // Initialize the whiteboard module.
+(function () {
+  'use strict';
 
-angular.module('whiteboard', ['ui.router'])
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('eraser', {
-        controller: 'toolbar'
-      });
-  })
-  // Set App to the root scope. 
-  .controller('canvas', function($rootScope, $scope, tools) {
-    $rootScope.app = App;
-  })
+  angular.module('whiteboard', ['ui.router'])
+    .config(function($stateProvider) {
+      $stateProvider
+        .state('eraser', {
+          controller: 'toolbar'
+        });
+    })
+    // Set App to the root scope. 
+    .controller('canvas', function($rootScope, $scope, tools) {
+      $rootScope.app = App;
+    })
+    // Set toolbar for colour palette and eraser. 
+    .controller('toolbar', toolBar)
+    .controller('switchBoardsController', switchBoardsCtrl)
+    // Set changePen method.
+    // Note that an eraser is simply a white pen, not actually erasing [x,y] tuples from the database. 
+    .service('tools', tools);
 
-// Set toolbar for colour palette and eraser. 
-.controller('toolbar', function($scope, $element, tools) {
-  $scope.changePen = function(option) {
-    tools.changePen(option);
-    console.log("The user chose the tool", $element);
-    $('input').not($('#' + option)).attr('checked', false);
-  };
-})
+  function tools ($rootScope) {
+    var changePen = function(option) {
+      if (option === 'eraser') {
+        console.log("The user is using the eraser.");
+        $rootScope.app.pen.lineWidth = 50;
+        $rootScope.app.pen.strokeStyle = '#fff';
+      } else {
+        console.log("The user is using the pen.");
+        $rootScope.app.pen.lineWidth = 5;
+        $rootScope.app.pen.strokeStyle = option;
+      }
+    };
+    return {
+      changePen: changePen
+    };
+  }
 
-.controller('switchBoardsController', function ($http, $location) {
-  console.log('hello');
+  function toolBar ($element, tools) {
+    var self = this;
+    self.changePen = function (option) {
+      tools.changePen(option);
+      console.log("The user chose the tool", $element);
+      $('input').not($('#' + option)).attr('checked', false);
+    };
+  }
 
-  var self = this;
+  function switchBoardsCtrl ($http, $location) {
+    var self = this;
+    self.switchBoards = function () {
+      console.log('running');
+      console.log('/' + self.boardId);
+      document.location = '/' + self.boardId;
+    };
+  }
 
-  self.switchBoards = function () {
-    console.log('running');
-    console.log('/' + self.boardId);
-    document.location = '/' + self.boardId;
-  };
-})
-
-// Set changePen method.
-// Note that an eraser is simply a white pen, not actually erasing [x,y] tuples from the database. 
-.service('tools', function($rootScope) {
-  var changePen = function(option) {
-    console.log($rootScope.app.pen);
-    if (option === 'eraser') {
-      console.log("The user is using the eraser.");
-      $rootScope.app.pen.lineWidth = 50;
-      $rootScope.app.pen.strokeStyle = '#fff';
-    } else {
-      console.log("The user is using the pen.");
-      $rootScope.app.pen.lineWidth = 5;
-      $rootScope.app.pen.strokeStyle = option;
-    }
-  };
-  return {
-    changePen: changePen
-  };
-
-});
+})();
