146aa79b4266af0f3bfdeff04ec35f796a2c72e2
Aaron Ackerman <a.ackerman050@gmail.com>
2015-11-23 13:48:39 -0800

    Implemented new firepad js for each room.

--- public/firepad.js
+++ public/firepad.js
@@ -1,7 +1,8 @@
-var firepadRef = new firebase('burning-inferno-4771.firebaseIO.com');
-var codeMirror = codeMirror(document.getElementById('firepad'), { lineWrapping: true });
+var firepadRef = new Firebase('burning-inferno-4771.firebaseIO.com');
+var codeMirror = CodeMirror(document.getElementById('firepad'), {
+  lineNumbers: true,
+  mode: 'javascript'
+});
 var firepad = Firepad.fromCodeMirror(firepadRef, codeMirror, {
-    richTextShortcuts: true,
-    richTextToolbar: true,
-    defaultText: 'Hello, World!' 
-  });
+  defaultText: 'JavaScript Editing with Firepad!' 
+});
